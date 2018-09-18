b263554eecc3804d5f8dcc93dc69b2f2eb5a69c6
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-18 09:15:41 -0500

    Part 1

---
+++ javascript/app.js
@@ -0,0 +1,34 @@
+console.log("up and running");
+// 1.
+// Create an object lightSwitch with a property isOn = true or false
+	// const lightSwitch = {
+		// isOn: true
+	// }
+// Create an object user with a property for email, which could be set to a prompt in the window where the user must enter their email address
+	// const user = {
+	// 	name: "Andy",
+	// 	email: "aprudhomme9@gmail.com" or window.prompt("enter your email", "your email here")
+	// }
+// Maybe create an array spaceShip with a list of all of its parts or an object that has the parts as properties or methods with corresponding values
+	// const spaceShip = ["hull", "laser blasters", "tractor beam", "warp drive"]
+	// or const spaceShip = {
+	// 	hull: "open",
+	// 	laser blasters: "on",
+	// 	tractorBeam () {
+	// 		console.log("tractor beeeaaamm")
+	// 	},
+	// 	warpDrive () {
+	// 		console.log("full speed ahead!")
+	// 	}
+	// }
+// An array, students, that lists all student names in the class
+// const students = ["Andy", "Chris R", "Chris L", "Charlotte", "Greg", "Ashley", "Fil", "Josh", "John"]
+// An array of objects with each student and corresponding location property
+// const students = [{name: "Andy", location: "Chicago"}, {name: "Chris R", locatin: "Chicago"}]
+// An array of objects, each of which contains a location property and then a favorite shows property that contains an array of shows
+// const students = [{
+// 	name: "Andy",
+// 	location: "Chicago",
+// 	favorite shows: ["The Office", "Parks and Rec", "Breaking Bad"]
+// }]
+
